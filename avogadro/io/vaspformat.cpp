1ab2879b7813f8f01d16ea0762d4988bc10ad024
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2021-06-21 19:16:21 +0900

    refactor(FileFormat): use std::unique_ptr
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- avogadro/io/vaspformat.cpp
+++ avogadro/io/vaspformat.cpp
@@ -202,11 +202,10 @@ bool PoscarFormat::read(std::istream& inStream, Core::Molecule& mol)
   }
 
   // Let's make a unit cell
-  auto* cell = new UnitCell(cellMat);
+  auto cell = std::make_unique<UnitCell>(cellMat);
 
   if (!cell->isRegular()) {
     appendError("cell vectors are not linear independent");
-    delete cell;
     return false;
   }
 
@@ -225,7 +224,7 @@ bool PoscarFormat::read(std::istream& inStream, Core::Molecule& mol)
   // Delete the current molecule. Add the new title and unit cell
   mol.clearAtoms();
   mol.setData("name", title);
-  mol.setUnitCell(cell);
+  mol.setUnitCell(cell.release());
 
   // Now add the atoms
   size_t k = 0;
