aa61fe2f5448edd431a05484dd56032e02a0f58f
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-07-02 15:52:43 +0900

    avogadro/io/lammpsformat.cpp

--- avogadro/io/lammpsformat.cpp
+++ avogadro/io/lammpsformat.cpp
@@ -501,7 +501,7 @@ bool LammpsDataFormat::write(std::ostream& outStream, const Core::Molecule& mol)
     massStream << iter->second << "   " << Elements::mass(iter->first) << "\n";
     ++iter;
   }
-  massStream << '\n' << '\n' << '\n';
+  massStream << "\n\n\n";
 
   if (numAtoms) {
     // Atomic coordinates
@@ -541,7 +541,7 @@ bool LammpsDataFormat::write(std::ostream& outStream, const Core::Molecule& mol)
       ++outputIdx;
     }
 
-    atomStream << '\n' << '\n';
+    atomStream << "\n\n";
   }
 
   if (numBonds) {
@@ -619,7 +619,7 @@ bool LammpsDataFormat::write(std::ostream& outStream, const Core::Molecule& mol)
              zmax - 0.5, 0.0, 0.0, 0.0);
     outStream << simBoxBlock;
   }
-  outStream << '\n' << '\n' << '\n';
+  outStream << "\n\n\n";
   outStream << massStream.str();
   outStream << atomStream.str();
   outStream << bondStream.str();
