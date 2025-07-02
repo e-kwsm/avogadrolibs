b8d503cd60739280ab06b970094362a4ab4a73b4
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-07-03 06:40:35 +0900

    avogadro/core/ringperceiver.cpp avogadro/rendering/arrowgeometry.h avogadro/rendering/drawable.h avogadro/rendering/linestripgeometry.h avogadro/rendering/meshgeometry.h

--- avogadro/rendering/meshgeometry.h
+++ avogadro/rendering/meshgeometry.h
@@ -49,7 +49,7 @@ public:
   ~MeshGeometry() override;
 
   MeshGeometry& operator=(MeshGeometry);
-  friend void swap(MeshGeometry& lhs, MeshGeometry& rhs);
+  friend void swap(MeshGeometry& lhs, MeshGeometry& rhs) noexcept;
 
   /**
    * Accept a visit from our friendly visitor.
@@ -160,7 +160,7 @@ inline MeshGeometry& MeshGeometry::operator=(MeshGeometry other)
   return *this;
 }
 
-inline void swap(MeshGeometry& lhs, MeshGeometry& rhs)
+inline void swap(MeshGeometry& lhs, MeshGeometry& rhs) noexcept
 {
   using std::swap;
   swap(static_cast<Drawable&>(lhs), static_cast<Drawable&>(rhs));
