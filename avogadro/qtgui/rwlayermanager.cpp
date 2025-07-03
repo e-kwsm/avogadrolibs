ca0f9d00c0e7964fc8d87063d39e568b3f3aa536
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-07-03 15:31:30 +0900

    avogadro/qtgui/insertfragmentdialog.cpp avogadro/qtgui/insertfragmentdialog.h avogadro/qtgui/molecule.cpp avogadro/qtgui/molecule.h avogadro/qtgui/pythonscript.cpp avogadro/qtgui/pythonscript.h avogadro/qtgui/rwlayermanager.cpp avogadro/qtgui/rwmolecule.cpp avogadro/qtgui/rwmolecule.h avogadro/qtgui/rwmolecule_undo.h avogadro/qtgui/utilities.cpp avogadro/qtgui/utilities.h avogadro/rendering/spheregeometry.h

--- avogadro/qtgui/rwlayermanager.cpp
+++ avogadro/qtgui/rwlayermanager.cpp
@@ -12,6 +12,7 @@
 #include <QUndoCommand>
 #include <QUndoStack>
 #include <cassert>
+#include <utility>
 
 namespace Avogadro::QtGui {
 
@@ -29,7 +30,7 @@ class AddLayerCommand : public QUndoCommand
 {
 public:
   AddLayerCommand(shared_ptr<MoleculeInfo> mol)
-    : QUndoCommand(QObject::tr("Modify Layers")), m_moleculeInfo(mol)
+    : QUndoCommand(QObject::tr("Modify Layers")), m_moleculeInfo(std::move(mol))
   {
     m_visible = true;
     m_locked = false;
@@ -110,8 +111,8 @@ class ActiveLayerCommand : public QUndoCommand
 {
 public:
   ActiveLayerCommand(shared_ptr<MoleculeInfo> mol, size_t layer)
-    : QUndoCommand(QObject::tr("Modify Layers")), m_moleculeInfo(mol),
-      m_newActiveLayer(layer)
+    : QUndoCommand(QObject::tr("Modify Layers")),
+      m_moleculeInfo(std::move(mol)), m_newActiveLayer(layer)
   {
     m_oldActiveLayer = m_moleculeInfo->layer.activeLayer();
   }
@@ -136,8 +137,8 @@ class RemoveLayerCommand : public QUndoCommand
 {
 public:
   RemoveLayerCommand(shared_ptr<MoleculeInfo> mol, size_t layer)
-    : QUndoCommand(QObject::tr("Modify Layers")), m_moleculeInfo(mol),
-      m_layer(layer)
+    : QUndoCommand(QObject::tr("Modify Layers")),
+      m_moleculeInfo(std::move(mol)), m_layer(layer)
   {
   }
 
