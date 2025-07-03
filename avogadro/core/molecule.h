0f81042237b90cad39f132a67b597b2b77df98a2
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-07-03 15:11:40 +0900

    avogadro/core/layermanager.h avogadro/core/molecule.cpp avogadro/core/molecule.h avogadro/core/neighborperceiver.cpp avogadro/core/neighborperceiver.h avogadro/core/residue.cpp avogadro/core/residue.h avogadro/core/variant-inline.h

--- avogadro/core/molecule.h
+++ avogadro/core/molecule.h
@@ -24,6 +24,7 @@
 #include <list>
 #include <map>
 #include <string>
+#include <utility>
 
 namespace Avogadro::Core {
 class BasisSet;
@@ -341,7 +342,7 @@ public:
 
   /**  Adds an atom to the molecule. */
   virtual AtomType addAtom(unsigned char atomicNumber);
-  AtomType addAtom(unsigned char atomicNumber, Vector3 position3d);
+  AtomType addAtom(unsigned char atomicNumber, const Vector3& position3d);
 
   /**
    * @brief Remove the specified atom from the molecule.
@@ -996,7 +997,7 @@ inline bool Molecule::setColor(Index atomId, Vector3ub color)
         m_colors.push_back(Vector3ub(Elements::color(atomicNumber(i))));
       }
     }
-    m_colors[atomId] = color;
+    m_colors[atomId] = std::move(color);
     return true;
   }
   return false;
