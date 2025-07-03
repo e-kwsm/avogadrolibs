60b5f642f716ae72216d0b5567e0da6389e1d04f
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-07-03 15:31:30 +0900

    avogadro/qtgui/insertfragmentdialog.cpp avogadro/qtgui/insertfragmentdialog.h avogadro/qtgui/molecule.cpp avogadro/qtgui/molecule.h avogadro/qtgui/pythonscript.cpp avogadro/qtgui/pythonscript.h avogadro/qtgui/rwlayermanager.cpp avogadro/qtgui/rwmolecule.cpp avogadro/qtgui/rwmolecule.h avogadro/qtgui/rwmolecule_undo.h avogadro/qtgui/utilities.cpp avogadro/qtgui/utilities.h avogadro/rendering/spheregeometry.h

--- avogadro/qtgui/utilities.cpp
+++ avogadro/qtgui/utilities.cpp
@@ -22,7 +22,7 @@ QString dataDirectory()
   return QString(AvogadroLibs_DATA_DIR);
 }
 
-QString findExecutablePath(QString program)
+QString findExecutablePath(const QString& program)
 {
   // we want to return the path to a program if it exists
   // using the PATH system environment variable
@@ -54,7 +54,7 @@ QString findExecutablePath(QString program)
   return QString();
 }
 
-QStringList findExecutablePaths(QStringList programs)
+QStringList findExecutablePaths(const QStringList& programs)
 {
   QStringList result;
 
