080e11497f3a4ff2cd05aacd0d951fb6b88da408
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-07-03 15:19:15 +0900

    avogadro/

--- avogadro/io/cjsonformat.cpp
+++ avogadro/io/cjsonformat.cpp
@@ -812,7 +812,7 @@ bool CjsonFormat::deserialize(std::istream& file, Molecule& molecule,
         }
         if (element.value().type() == json::value_t::array) {
           // check if it is a numeric array to go into Eigen::MatrixXd
-          json j = element.value(); // convenience
+          const json& j = element.value(); // convenience
           std::size_t rows = j.size();
           MatrixX matrix;
           matrix.resize(rows, 1); // default to 1 columns
@@ -1024,7 +1024,7 @@ bool CjsonFormat::serialize(std::ostream& file, const Molecule& molecule,
   properties["totalCharge"] = molecule.totalCharge();
   properties["totalSpinMultiplicity"] = molecule.totalSpinMultiplicity();
   // loop through all other properties
-  const auto map = molecule.dataMap();
+  const auto& map = molecule.dataMap();
   for (const auto& element : map) {
     if (element.first == "name" || element.first == "inchi")
       continue;
@@ -1463,7 +1463,7 @@ bool CjsonFormat::serialize(std::ostream& file, const Molecule& molecule,
     atoms["coords"] = coords;
 
     // forces if present
-    const auto forceVectors = molecule.forceVectors();
+    const auto& forceVectors = molecule.forceVectors();
     if (forceVectors.size() == molecule.atomCount()) {
       json forces;
       for (const auto& force : forceVectors) {
