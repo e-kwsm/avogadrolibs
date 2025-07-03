2b56cd655cdfcc233e487ed84881b00948ac2d62
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-07-03 15:31:30 +0900

    avogadro/qtgui/insertfragmentdialog.cpp avogadro/qtgui/insertfragmentdialog.h avogadro/qtgui/molecule.cpp avogadro/qtgui/molecule.h avogadro/qtgui/pythonscript.cpp avogadro/qtgui/pythonscript.h avogadro/qtgui/rwlayermanager.cpp avogadro/qtgui/rwmolecule.cpp avogadro/qtgui/rwmolecule.h avogadro/qtgui/rwmolecule_undo.h avogadro/qtgui/utilities.cpp avogadro/qtgui/utilities.h avogadro/rendering/spheregeometry.h

--- avogadro/qtgui/pythonscript.cpp
+++ avogadro/qtgui/pythonscript.cpp
@@ -271,7 +271,7 @@ void PythonScript::asyncTerminate()
   }
 }
 
-QByteArray PythonScript::asyncWriteAndResponse(QByteArray input)
+QByteArray PythonScript::asyncWriteAndResponse(const QByteArray& input)
 {
   if (m_process == nullptr) {
     return QByteArray(); // wait
