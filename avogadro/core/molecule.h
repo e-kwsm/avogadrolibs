d7e34ca06e88abe2679aec5335f92a9397764524
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-07-04 11:48:45 +0900

    refactor(Molecule): shared_ptr for m_basisSet
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- avogadro/core/molecule.h
+++ avogadro/core/molecule.h
@@ -630,13 +630,13 @@ public:
    * Set the basis set for the molecule, note that the molecule takes ownership
    * of the object.
    */
-  void setBasisSet(BasisSet* basis) { m_basisSet = basis; }
+  void setBasisSet(BasisSet* basis);
 
   /**
    * @return the basis set (if present) for the molecule.
    */
-  BasisSet* basisSet() { return m_basisSet; }
-  const BasisSet* basisSet() const { return m_basisSet; }
+  BasisSet* basisSet() { return m_basisSet.get(); }
+  const BasisSet* basisSet() const { return m_basisSet.get(); }
 
   /**
    * The unit cell for this molecule. May be nullptr for non-periodic
@@ -1226,7 +1226,7 @@ protected:
   std::vector<Cube*> m_cubes;
   Index m_activeCubeIndex = 0;
 
-  BasisSet* m_basisSet;
+  std::shared_ptr<BasisSet> m_basisSet;
   UnitCell* m_unitCell;
   Array<Residue> m_residues;
 
