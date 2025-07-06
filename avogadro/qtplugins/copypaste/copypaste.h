c0af480ba215f6e8aa74bb01f83739a3e8449723
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-07-07 03:46:47 +0900

    refactor(CopyPaste): std::unique_ptr m_pastedFormat
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- avogadro/qtplugins/copypaste/copypaste.h
+++ avogadro/qtplugins/copypaste/copypaste.h
@@ -11,6 +11,8 @@
 
 #include <QtGui/QIcon>
 
+#include <memory>
+
 namespace Avogadro {
 namespace Io {
 class FileFormat;
@@ -55,7 +57,7 @@ private slots:
 private:
   // Cached between emitting moleculeReady() and calling readMolecule().
   QByteArray m_pastedData;
-  Io::FileFormat* m_pastedFormat;
+  std::unique_ptr<Io::FileFormat> m_pastedFormat;
 
   QtGui::Molecule* m_molecule;
 
