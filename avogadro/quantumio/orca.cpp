ed6e9097b8af25ae6290b572187d4315d21b081c
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-07-07 04:56:45 +0900

    avogadro/quantumio/gamessus.cpp avogadro/quantumio/gaussianfchk.cpp avogadro/quantumio/mopacaux.cpp avogadro/quantumio/nwchemjson.cpp avogadro/quantumio/orca.cpp

--- avogadro/quantumio/orca.cpp
+++ avogadro/quantumio/orca.cpp
@@ -47,10 +47,10 @@ std::vector<std::string> ORCAOutput::mimeTypes() const
 bool ORCAOutput::read(std::istream& in, Core::Molecule& molecule)
 {
   // Read the log file line by line
-  auto* basis = new GaussianSet;
+  auto basis = std::make_shared<GaussianSet>();
 
   while (!in.eof())
-    processLine(in, basis);
+    processLine(in, basis.get());
 
   // Set up the molecule
   int nAtom = 0;
@@ -159,7 +159,7 @@ bool ORCAOutput::read(std::istream& in, Core::Molecule& molecule)
 
   molecule.setBasisSet(basis);
   basis->setMolecule(&molecule);
-  load(basis);
+  load(basis.get());
 
   // we have to do a few things *after* any modifications to bonds / atoms
   // because those automatically clear partial charges and data
