ecc473828eabc491bc08853b134deefebe18b075
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-07-06 13:12:48 +0900

    ScriptCharges::m_models;
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- avogadro/qtplugins/scriptcharges/scriptcharges.cpp
+++ avogadro/qtplugins/scriptcharges/scriptcharges.cpp
@@ -39,7 +39,6 @@ void ScriptCharges::setMolecule(QtGui::Molecule*) {}
 void ScriptCharges::refreshModels()
 {
   unregisterModels();
-  qDeleteAll(m_models);
   m_models.clear();
 
   QMultiMap<QString, QString> scriptPaths =
