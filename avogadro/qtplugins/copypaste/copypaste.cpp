1497bb0f98cff22e65d073178c9e6a19b7627f06
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-07-07 03:46:47 +0900

    refactor(CopyPaste): std::unique_ptr m_pastedFormat
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- avogadro/qtplugins/copypaste/copypaste.cpp
+++ avogadro/qtplugins/copypaste/copypaste.cpp
@@ -77,10 +77,7 @@ CopyPaste::CopyPaste(QObject* parent_)
   connect(m_clearAction, SIGNAL(triggered()), SLOT(clear()));
 }
 
-CopyPaste::~CopyPaste()
-{
-  delete m_pastedFormat;
-}
+CopyPaste::~CopyPaste() = default;
 
 QList<QAction*> CopyPaste::actions() const
 {
@@ -262,8 +259,7 @@ void CopyPaste::paste()
 {
   // Delete any old clipboard data.
   if (m_pastedFormat) {
-    delete m_pastedFormat;
-    m_pastedFormat = nullptr;
+    m_pastedFormat.reset();
     m_pastedData.clear();
   }
 
@@ -288,8 +284,9 @@ void CopyPaste::paste()
   QStringList mimeTypes(mimeData->formats());
   Io::FileFormat::Operations ops(Io::FileFormat::Read | Io::FileFormat::String);
   foreach (const QString& mimeType, mimeTypes) {
-    if ((m_pastedFormat =
-           mgr.newFormatFromMimeType(mimeType.toStdString(), ops))) {
+    m_pastedFormat.reset(
+      mgr.newFormatFromMimeType(mimeType.toStdString(), ops));
+    if (m_pastedFormat) {
       m_pastedData = mimeData->data(mimeType);
       break;
     }
@@ -297,7 +294,7 @@ void CopyPaste::paste()
 
   // No mime-type match, default to cjson.
   if (!m_pastedFormat && mimeData->hasText()) {
-    m_pastedFormat = new Io::CjsonFormat;
+    m_pastedFormat = std::make_unique<Io::CjsonFormat>();
     m_pastedData = mimeData->text().toLatin1();
   }
 
@@ -325,8 +322,7 @@ void CopyPaste::paste()
   m_molecule->undoMolecule()->appendMolecule(mol, "Paste Molecule");
   emit requestActiveTool("Manipulator");
 
-  delete m_pastedFormat;
-  m_pastedFormat = nullptr;
+  m_pastedFormat.reset();
   m_pastedData.clear();
 }
 
