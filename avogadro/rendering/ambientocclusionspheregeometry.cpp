ab913c04764eb010c0381bfda78701c8cdeb7902
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-07-07 06:59:09 +0900

    refactor(AmbientOcclusionSphereGeometry):
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- avogadro/rendering/ambientocclusionspheregeometry.cpp
+++ avogadro/rendering/ambientocclusionspheregeometry.cpp
@@ -1091,10 +1091,7 @@ AmbientOcclusionSphereGeometry::AmbientOcclusionSphereGeometry(
 {
 }
 
-AmbientOcclusionSphereGeometry::~AmbientOcclusionSphereGeometry()
-{
-  delete d;
-}
+AmbientOcclusionSphereGeometry::~AmbientOcclusionSphereGeometry() = default;
 
 void AmbientOcclusionSphereGeometry::accept(Visitor& visitor)
 {
