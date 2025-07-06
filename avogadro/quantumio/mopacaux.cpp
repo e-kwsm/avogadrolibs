b231cbce30df8847049b5b8b66b5b2fca368a333
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-07-07 04:56:45 +0900

    avogadro/quantumio/gamessus.cpp avogadro/quantumio/gaussianfchk.cpp avogadro/quantumio/mopacaux.cpp avogadro/quantumio/nwchemjson.cpp avogadro/quantumio/orca.cpp

--- avogadro/quantumio/mopacaux.cpp
+++ avogadro/quantumio/mopacaux.cpp
@@ -46,7 +46,7 @@ bool MopacAux::read(std::istream& in, Core::Molecule& molecule)
   while (!in.eof())
     processLine(in);
 
-  auto* basis = new SlaterSet;
+  auto basis = std::make_shared<SlaterSet>();
 
   for (unsigned int i = 0; i < m_atomPos.size(); ++i) {
     Atom a = molecule.addAtom(static_cast<unsigned char>(m_atomNums[i]));
@@ -57,7 +57,7 @@ bool MopacAux::read(std::istream& in, Core::Molecule& molecule)
   molecule.perceiveBondOrders();
   molecule.setBasisSet(basis);
   basis->setMolecule(&molecule);
-  load(basis);
+  load(basis.get());
 
   // Bank the last Hessian, which has no following geometry to trigger it.
   flushVibrationData();
