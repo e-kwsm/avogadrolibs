165b7e8ddc666dd6136eb14655d0795653587ce4
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-07-08 13:45:21 +0900

    avogadro/qtplugins/openbabel/obcharges.cpp avogadro/qtplugins/openbabel/obfileformat.cpp

--- avogadro/qtplugins/openbabel/obcharges.cpp
+++ avogadro/qtplugins/openbabel/obcharges.cpp
@@ -26,7 +26,7 @@ class OBCharges::ProcessListener : public QObject
 {
   Q_OBJECT
 public:
-  ProcessListener() : QObject(), m_finished(false) {}
+  ProcessListener() : m_finished(false) {}
 
   bool waitForOutput(Array<double>& output, int msTimeout = 120000)
   {
@@ -62,7 +62,7 @@ private:
   Array<double> m_output;
 };
 
-OBCharges::OBCharges(const std::string& id) : ChargeModel(), m_identifier(id)
+OBCharges::OBCharges(const std::string& id) : m_identifier(id)
 {
   // set the element mask based on our type / identifier
   m_elements.reset();
