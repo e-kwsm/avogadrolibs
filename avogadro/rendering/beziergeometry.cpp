2429a7eaf507ab1b6d1db26d58c5e8f5d5ef0293
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-07-08 13:23:22 +0900

    fixup! avogadro/rendering/beziergeometry.cpp

--- avogadro/rendering/beziergeometry.cpp
+++ avogadro/rendering/beziergeometry.cpp
@@ -7,7 +7,7 @@
 
 namespace Avogadro::Rendering {
 
-BezierGeometry::BezierGeometry() : {}
+BezierGeometry::BezierGeometry() = default;
 
 Vector3f BezierGeometry::computeCurvePoint(
   float t, const std::list<Point*>& points) const
