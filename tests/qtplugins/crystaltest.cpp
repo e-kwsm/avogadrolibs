08584bb8bb34daafc6d17c2baad1729a4c3ce8e4
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-07-10 19:00:51 +0900

    tests/qtplugins/CMakeLists.txt tests/qtplugins/crystaltest.cpp

---
+++ tests/qtplugins/crystaltest.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+  This source file is part of the Avogadro project.
+  This source code is released under the 3-Clause BSD License, (see "LICENSE").
+******************************************************************************/
+
+// #include "qtguitests.h"
+#include <gtest/gtest.h>
+
+// #include <avogadro/qtgui/filebrowsewidget.h>
+
+// #include <QtTest/QSignalSpy>
+
+// #include <QtWidgets/QApplication>
+// #include <QtWidgets/QLineEdit>
+
+// #include <QtCore/QVariant>
+
+TEST(crystal, UnitCellEditor) {
+}
