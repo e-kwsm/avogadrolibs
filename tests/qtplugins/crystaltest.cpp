9da4549ee3761b58142b46e1d28ccea8fdebc2d3
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-07-13 15:07:11 +0900

    tests/qtplugins/crystaltest.cpp

--- tests/qtplugins/crystaltest.cpp
+++ tests/qtplugins/crystaltest.cpp
@@ -8,7 +8,7 @@
 
 #include <avogadro/qtplugins/crystal/unitcelldialog.h>
 
-// #include <QtTest/QSignalSpy>
+#include <QtTest/QSignalSpy>
 
 // #include <QtWidgets/QApplication>
 // #include <QtWidgets/QLineEdit>
@@ -18,6 +18,40 @@
 #include <avogadro/core/unitcell.h>
 
 using Avogadro::Core::UnitCell;
+
+// Need a QApplication to instantiate widget
+#define START_QAPP                                                             \
+  int argc = 1;                                                                \
+  char argName[] = "FakeApp.exe";                                              \
+  char* argv[2] = { argName, nullptr };                                        \
+  QApplication app(argc, argv);                                                \
+  Q_UNUSED(app)
+
+TEST(FileBrowseWidgetTest, setFileName)
+{
+  START_QAPP;
+
+#ifndef HOGE
+  using Avogadro::QtPlugins::UnitCellDialog;
+  UnitCellDialog dialog;
+  if (QT_VERSION == 6)
+    QSignalSpy spy(&dialog, &UnitCellDialog::parametersEdited);
+  else
+    QSignalSpy spy(&dialog, SIGNAL(&UnitCellDialog::parametersEdited));
+
+#else
+  FileBrowseWidget widget;
+  if (QT_VERSION == 6)
+    QSignalSpy spy(&widget, &FileBrowseWidget::fileNameChanged);
+  else
+    QSignalSpy spy(&widget, SIGNAL(fileNameChanged(QString)));
+  widget.setFileName("some file");
+  EXPECT_EQ(1, spy.count());
+  EXPECT_STREQ("some file", qPrintable(spy.front().front().toString()));
+  EXPECT_STREQ("some file", qPrintable(widget.fileName()));
+  EXPECT_STREQ("some file", qPrintable(widget.lineEdit()->text()));
+#endif
+}
 
 class CrystalTest : public testing::Test
 {
