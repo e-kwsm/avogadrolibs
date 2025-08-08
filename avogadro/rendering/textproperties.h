2dd39b331359ad42f8c2d259b430205f73a1752f
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-08 23:25:18 +0900

    avogadro/rendering/textproperties.h

--- avogadro/rendering/textproperties.h
+++ avogadro/rendering/textproperties.h
@@ -169,7 +169,7 @@ private:
   float m_rotationDegreesCW;
   FontFamily m_fontFamily;
   FontStyles m_fontStyles;
-  unsigned char m_rgba[4];
+  std::array<unsigned char, 4> m_rgba;
 };
 
 inline void TextProperties::setAlign(TextProperties::HAlign h,
