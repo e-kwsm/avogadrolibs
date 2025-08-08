9075e49ca895dd1cb31f4edd7599367bf6059e54
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-08 23:22:39 +0900

    tests/io/cjsontest.cpp
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- tests/io/cjsontest.cpp
+++ tests/io/cjsontest.cpp
@@ -197,9 +197,8 @@ TEST(CjsonTest, saveFile)
   EXPECT_EQ(bond.order(), static_cast<unsigned char>(1));
 }
 
-/*
 // For now, disable this test until we can find the leak
-TEST(CjsonTest, conformers)
+TEST(DISABLED_CjsonTest, conformers)
 {
   CjsonFormat cjson;
   Molecule molecule;
@@ -224,4 +223,3 @@ TEST(CjsonTest, conformers)
   EXPECT_EQ(otherMolecule.bondCount(), static_cast<size_t>(13));
   EXPECT_EQ(otherMolecule.coordinate3dCount(), static_cast<size_t>(3));
 }
-*/
