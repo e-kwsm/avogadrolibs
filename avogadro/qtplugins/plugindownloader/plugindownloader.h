ba570ef9510131aedbbc76f0eb41fc07907ce346
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-09 13:07:25 +0900

    refactor: setMolecule shared_ptr

--- avogadro/qtplugins/plugindownloader/plugindownloader.h
+++ avogadro/qtplugins/plugindownloader/plugindownloader.h
@@ -45,6 +45,7 @@ public:
   QStringList menuPath(QAction*) const override;
 
   void setMolecule(QtGui::Molecule*) override {}
+  void setMolecule(const std::shared_ptr<QtGui::Molecule>&) override {}
 
 private slots:
   void showDialog();
