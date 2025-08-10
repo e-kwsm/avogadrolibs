3bfb6d0dad392ad7b0ea26a246bf969bf68b276b
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-11 00:45:13 +0900

    avogadro/qtplugins/cartoons/cartoons.cpp avogadro/qtplugins/coordinateeditor/coordinatetextedit.h avogadro/qtplugins/openbabel/obfileformat.cpp avogadro/qtplugins/openbabel/obfileformat.h avogadro/qtplugins/svg/svg.h

--- avogadro/qtplugins/openbabel/obfileformat.cpp
+++ avogadro/qtplugins/openbabel/obfileformat.cpp
@@ -14,6 +14,7 @@
 #include <QtCore/QFileInfo>
 #include <QtCore/QTemporaryFile>
 #include <QtCore/QTimer>
+#include <utility>
 
 using namespace std::string_literals;
 using json = nlohmann::json;
@@ -63,18 +64,17 @@ private:
   QByteArray m_output;
 };
 
-OBFileFormat::OBFileFormat(const std::string& name_,
-                           const std::string& identifier_,
-                           const std::string& description_,
-                           const std::string& specificationUrl_,
+OBFileFormat::OBFileFormat(std::string name_, std::string identifier_,
+                           std::string description_,
+                           std::string specificationUrl_,
                            const std::vector<std::string> fileExtensions_,
                            const std::vector<std::string> mimeTypes_,
-                           const std::string& defaultFormat_, bool fileOnly_)
-  : Io::FileFormat(), m_description(description_),
+                           std::string defaultFormat_, bool fileOnly_)
+  : Io::FileFormat(), m_description(std::move(description_)),
     m_fileExtensions(fileExtensions_), m_mimeTypes(mimeTypes_),
-    m_identifier(identifier_), m_name(name_),
-    m_specificationUrl(specificationUrl_), m_defaultFormat(defaultFormat_),
-    m_fileOnly(fileOnly_)
+    m_identifier(std::move(identifier_)), m_name(std::move(name_)),
+    m_specificationUrl(std::move(specificationUrl_)),
+    m_defaultFormat(std::move(defaultFormat_)), m_fileOnly(fileOnly_)
 {
 }
 
