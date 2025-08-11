ef54ebc4e81e1b0a42541fb517dada97dcb4dcfc
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-12 04:34:05 +0900

    tests/io/cmltest.cpp

--- tests/io/cmltest.cpp
+++ tests/io/cmltest.cpp
@@ -132,21 +132,27 @@ TEST(CmlTest, fractionalCoords)
   cmlStr.clear();
   cml.writeString(cmlStr, molecule);
   std::cout << cmlStr << std::endl;
-  EXPECT_TRUE(cmlStr.find("<scalar title=\"a\" units=\"units:angstrom\">5.3"
-                          "</scalar>") != std::string::npos);
-  EXPECT_TRUE(cmlStr.find("<scalar title=\"b\" units=\"units:angstrom\">2.4"
-                          "</scalar>") != std::string::npos);
-  EXPECT_TRUE(cmlStr.find("<scalar title=\"c\" units=\"units:angstrom\">1.8"
-                          "</scalar>") != std::string::npos);
-  EXPECT_TRUE(cmlStr.find("<scalar title=\"alpha\" units=\"units:degree\">85"
-                          "</scalar>") != std::string::npos);
-  EXPECT_TRUE(cmlStr.find("<scalar title=\"beta\" units=\"units:degree\">90"
-                          "</scalar>") != std::string::npos);
-  EXPECT_TRUE(cmlStr.find("<scalar title=\"gamma\" units=\"units:degree\">105"
-                          "</scalar>") != std::string::npos);
-  EXPECT_TRUE(cmlStr.find("xFract=\"0.5\"") != std::string::npos);
-  EXPECT_TRUE(cmlStr.find("yFract=\"0.5\"") != std::string::npos);
-  EXPECT_TRUE(cmlStr.find("zFract=\"0.5\"") != std::string::npos);
+  EXPECT_NE(cmlStr.find("<scalar title=\"a\" units=\"units:angstrom\">5.3"
+                        "</scalar>"),
+            std::string::npos);
+  EXPECT_NE(cmlStr.find("<scalar title=\"b\" units=\"units:angstrom\">2.4"
+                        "</scalar>"),
+            std::string::npos);
+  EXPECT_NE(cmlStr.find("<scalar title=\"c\" units=\"units:angstrom\">1.8"
+                        "</scalar>"),
+            std::string::npos);
+  EXPECT_NE(cmlStr.find("<scalar title=\"alpha\" units=\"units:degree\">85"
+                        "</scalar>"),
+            std::string::npos);
+  EXPECT_NE(cmlStr.find("<scalar title=\"beta\" units=\"units:degree\">90"
+                        "</scalar>"),
+            std::string::npos);
+  EXPECT_NE(cmlStr.find("<scalar title=\"gamma\" units=\"units:degree\">105"
+                        "</scalar>"),
+            std::string::npos);
+  EXPECT_NE(cmlStr.find("xFract=\"0.5\""), std::string::npos);
+  EXPECT_NE(cmlStr.find("yFract=\"0.5\""), std::string::npos);
+  EXPECT_NE(cmlStr.find("zFract=\"0.5\""), std::string::npos);
 }
 
 TEST(CmlTest, saveFile)
