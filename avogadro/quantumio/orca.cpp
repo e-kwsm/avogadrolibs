6a27990efb306b4e190f479bf7f15ee721ffbb9c
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-20 11:43:48 +0900

    refactor: use initializer list
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- avogadro/quantumio/orca.cpp
+++ avogadro/quantumio/orca.cpp
@@ -146,14 +146,12 @@ void ORCAOutput::flushVibrationData()
 
 std::vector<std::string> ORCAOutput::fileExtensions() const
 {
-  std::vector<std::string> extensions;
-  extensions.emplace_back("orca");
-  return extensions;
+  return { "orca" };
 }
 
 std::vector<std::string> ORCAOutput::mimeTypes() const
 {
-  return std::vector<std::string>();
+  return {};
 }
 
 bool ORCAOutput::read(std::istream& in, Core::Molecule& molecule)
