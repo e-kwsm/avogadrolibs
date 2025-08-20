9296787e47e939879b26abccc0e1697f6380d364
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-20 11:24:51 +0900

    avogadro/quantumio/genericoutput.cpp avogadro/quantumio/genericoutput.h

--- avogadro/quantumio/genericoutput.h
+++ avogadro/quantumio/genericoutput.h
@@ -33,8 +33,8 @@ public:
 
   std::string specificationUrl() const override { return ""; }
 
-  std::vector<std::string> fileExtensions() const override;
-  std::vector<std::string> mimeTypes() const override;
+  std::set<std::string> fileExtensions() const override;
+  std::set<std::string> mimeTypes() const override;
 
   bool read(std::istream& in, Core::Molecule& molecule) override;
   bool write(std::ostream&, const Core::Molecule&) override
