19f330ed18176c068642de66faf460484bc23e39
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-20 11:22:59 +0900

    avogadro/io/dcdformat.cpp avogadro/io/dcdformat.h avogadro/io/gromacsformat.cpp avogadro/io/gromacsformat.h

--- avogadro/io/gromacsformat.h
+++ avogadro/io/gromacsformat.h
@@ -42,8 +42,8 @@ public:
            "file-formats.html#gro";
   }
 
-  std::vector<std::string> fileExtensions() const override;
-  std::vector<std::string> mimeTypes() const override;
+  std::set<std::string> fileExtensions() const override;
+  std::set<std::string> mimeTypes() const override;
 
   [[nodiscard]] bool read(std::istream& in, Core::Molecule& molecule) override;
 
