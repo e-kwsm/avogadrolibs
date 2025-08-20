cf37aae935067e52e921d81e389f3baa18ab2454
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-20 11:27:04 +0900

    avogadro/quantumio/gaussiancube.cpp avogadro/quantumio/gaussiancube.h avogadro/quantumio/gaussianfchk.cpp avogadro/quantumio/gaussianfchk.h

--- avogadro/quantumio/gaussiancube.h
+++ avogadro/quantumio/gaussiancube.h
@@ -38,8 +38,8 @@ public:
     return "https://gaussian.com/cubegen/";
   }
 
-  std::vector<std::string> fileExtensions() const override;
-  std::vector<std::string> mimeTypes() const override;
+  std::set<std::string> fileExtensions() const override;
+  std::set<std::string> mimeTypes() const override;
 
   [[nodiscard]] bool read(std::istream& in, Core::Molecule& molecule) override;
   [[nodiscard]] bool write(std::ostream& out,
