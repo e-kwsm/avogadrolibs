d81f6edbec3808ab8423c4848b441ded62c0a0e1
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-20 11:36:35 +0900

    avogadro/qtplugins/openbabel/obfileformat.cpp avogadro/qtplugins/scriptfileformats/fileformatscript.h

--- avogadro/qtplugins/scriptfileformats/fileformatscript.h
+++ avogadro/qtplugins/scriptfileformats/fileformatscript.h
@@ -152,12 +152,12 @@ public:
 
   std::string specificationUrl() const override { return m_specificationUrl; }
 
-  std::vector<std::string> fileExtensions() const override
+  std::set<std::string> fileExtensions() const override
   {
     return m_fileExtensions;
   }
 
-  std::vector<std::string> mimeTypes() const override { return m_mimeTypes; }
+  std::set<std::string> mimeTypes() const override { return m_mimeTypes; }
 
 private:
   static Format stringToFormat(const std::string& str);
@@ -179,8 +179,8 @@ private:
   std::string m_name;
   std::string m_description;
   std::string m_specificationUrl;
-  std::vector<std::string> m_fileExtensions;
-  std::vector<std::string> m_mimeTypes;
+  std::set<std::string> m_fileExtensions;
+  std::set<std::string> m_mimeTypes;
 };
 
 } // namespace QtPlugins
