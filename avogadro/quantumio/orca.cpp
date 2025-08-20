e66fe26de739ea067319acca0e87448fefa3acd2
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-20 11:43:48 +0900

    refactor: use initializer list
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- avogadro/quantumio/orca.cpp
+++ avogadro/quantumio/orca.cpp
@@ -116,14 +116,12 @@ std::string nthSpaceToken(const std::string& line, std::size_t index)
 
 std::vector<std::string> ORCAOutput::fileExtensions() const
 {
-  std::vector<std::string> extensions;
-  extensions.emplace_back("orca");
-  return extensions;
+  return { "orca" };
 }
 
 std::vector<std::string> ORCAOutput::mimeTypes() const
 {
-  return std::vector<std::string>();
+  return {};
 }
 
 bool ORCAOutput::read(std::istream& in, Core::Molecule& molecule)
