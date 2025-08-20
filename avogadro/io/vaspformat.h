dbb7ff4fdd7ad8a18d54206beb5b90c0623d6bb0
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-20 11:09:08 +0900

    avogadro/io/fileformat.h
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- avogadro/io/vaspformat.h
+++ avogadro/io/vaspformat.h
@@ -40,8 +40,8 @@ public:
     return "https://www.vasp.at/wiki/POSCAR";
   }
 
-  std::vector<std::string> fileExtensions() const override;
-  std::vector<std::string> mimeTypes() const override;
+  std::set<std::string> fileExtensions() const override;
+  std::set<std::string> mimeTypes() const override;
 
   [[nodiscard]] bool read(std::istream& inStream, Core::Molecule& mol) override;
   [[nodiscard]] bool write(std::ostream& outStream,
