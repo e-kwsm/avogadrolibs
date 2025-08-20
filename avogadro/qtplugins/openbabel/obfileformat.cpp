172f879e615ed801c2b4560ed6da6dbeb1e183a4
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-20 11:38:43 +0900

    avogadro/qtplugins/openbabel/obfileformat.cpp avogadro/qtplugins/scriptfileformats/fileformatscript.cpp avogadro/qtplugins/scriptfileformats/fileformatscript.h

--- avogadro/qtplugins/openbabel/obfileformat.cpp
+++ avogadro/qtplugins/openbabel/obfileformat.cpp
@@ -274,7 +274,7 @@ bool OBFileFormat::write(std::ostream& out, const Core::Molecule& molecule)
   }
 
   proc.convert(QByteArray(outputString.c_str()), m_defaultFormat.c_str(),
-               QString::fromStdString(m_fileExtensions.front()), options);
+               QString::fromStdString(*m_fileExtensions.begin()), options);
 
   QByteArray output;
   if (!listener.waitForOutput(output)) {
