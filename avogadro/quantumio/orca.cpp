a36aee61586ca6c33b9713ed377b40d76ebac619
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-20 11:28:54 +0900

    avogadro/quantumio/mopacaux.cpp avogadro/quantumio/mopacaux.h avogadro/quantumio/nwchemjson.cpp avogadro/quantumio/nwchemjson.h avogadro/quantumio/nwchemlog.cpp avogadro/quantumio/nwchemlog.h avogadro/quantumio/orca.cpp avogadro/quantumio/orca.h

--- avogadro/quantumio/orca.cpp
+++ avogadro/quantumio/orca.cpp
@@ -144,16 +144,16 @@ void ORCAOutput::flushVibrationData()
   m_haveNormalModes = false;
 }
 
-std::vector<std::string> ORCAOutput::fileExtensions() const
+std::set<std::string> ORCAOutput::fileExtensions() const
 {
-  std::vector<std::string> extensions;
-  extensions.emplace_back("orca");
+  std::set<std::string> extensions;
+  extensions.insert("orca");
   return extensions;
 }
 
-std::vector<std::string> ORCAOutput::mimeTypes() const
+std::set<std::string> ORCAOutput::mimeTypes() const
 {
-  return std::vector<std::string>();
+  return {};
 }
 
 bool ORCAOutput::read(std::istream& in, Core::Molecule& molecule)
