21253ca472f3e6cb64fa9b137717cd7549b88dc1
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-20 11:21:47 +0900

    avogadro/io/turbomoleformat.h avogadro/io/vaspformat.cpp avogadro/io/vaspformat.h avogadro/io/xyzformat.cpp avogadro/io/xyzformat.h

--- avogadro/io/turbomoleformat.h
+++ avogadro/io/turbomoleformat.h
@@ -44,8 +44,8 @@ public:
            "geometry.html#turbomole-coordinate-input";
   }
 
-  std::vector<std::string> fileExtensions() const override;
-  std::vector<std::string> mimeTypes() const override;
+  std::set<std::string> fileExtensions() const override;
+  std::set<std::string> mimeTypes() const override;
 
   bool read(std::istream& inStream, Core::Molecule& molecule) override;
   bool write(std::ostream& outStream, const Core::Molecule& molecule) override;
