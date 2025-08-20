44bf724e669a2071cda36cf4815625cf82644cce
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-20 11:17:04 +0900

    avogadro/io/cmsgpackformat.cpp avogadro/io/cmsgpackformat.h

--- avogadro/io/cmsgpackformat.h
+++ avogadro/io/cmsgpackformat.h
@@ -47,8 +47,8 @@ public:
     return "https://github.com/openchemistry/chemicaljson";
   }
 
-  std::vector<std::string> fileExtensions() const override;
-  std::vector<std::string> mimeTypes() const override;
+  std::set<std::string> fileExtensions() const override;
+  std::set<std::string> mimeTypes() const override;
 
   bool read(std::istream& in, Core::Molecule& molecule) override;
   bool write(std::ostream& out, const Core::Molecule& molecule) override;
