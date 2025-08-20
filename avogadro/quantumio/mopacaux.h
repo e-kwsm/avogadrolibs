cc03642cd52ae94d4ec106f786e43b36ff5085ed
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-20 11:28:54 +0900

    avogadro/quantumio/mopacaux.cpp avogadro/quantumio/mopacaux.h avogadro/quantumio/nwchemjson.cpp avogadro/quantumio/nwchemjson.h avogadro/quantumio/nwchemlog.cpp avogadro/quantumio/nwchemlog.h avogadro/quantumio/orca.cpp avogadro/quantumio/orca.h

--- avogadro/quantumio/mopacaux.h
+++ avogadro/quantumio/mopacaux.h
@@ -38,8 +38,8 @@ public:
     return "http://openmopac.net/manual/auxiliary.html";
   }
 
-  std::vector<std::string> fileExtensions() const override;
-  std::vector<std::string> mimeTypes() const override;
+  std::set<std::string> fileExtensions() const override;
+  std::set<std::string> mimeTypes() const override;
 
   bool read(std::istream& in, Core::Molecule& molecule) override;
   bool write(std::ostream&, const Core::Molecule&) override
