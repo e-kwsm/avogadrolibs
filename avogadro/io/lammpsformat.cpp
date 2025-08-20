41c760b81cc43a8b2964e80479d84fd5d2183a8c
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-08-20 12:23:27 +0900

    refactor: use std::minmax
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- avogadro/io/lammpsformat.cpp
+++ avogadro/io/lammpsformat.cpp
@@ -98,10 +98,9 @@ bool LammpsTrajectoryFormat::read(std::istream& inStream, Core::Molecule& mol)
     z_max = lexicalCast<double>(box_bounds_z.at(1)).value_or(0.0);
     tilt_yz = lexicalCast<double>(box_bounds_z.at(2)).value_or(0.0);
 
-    x_min -= std::min(std::min(std::min(tilt_xy, tilt_xz), tilt_xy + tilt_xz),
-                      (double)0);
-    x_max -= std::max(std::max(std::max(tilt_xy, tilt_xz), tilt_xy + tilt_xz),
-                      (double)0);
+    auto tmp = std::minmax({ tilt_xy, tilt_xz, tilt_xy + tilt_xz, 0.0 });
+    x_min -= tmp.first;
+    x_max -= tmp.second;
     y_min -= std::min(tilt_yz, (double)0);
     y_max -= std::max(tilt_yz, (double)0);
   }
@@ -299,10 +298,9 @@ bool LammpsTrajectoryFormat::read(std::istream& inStream, Core::Molecule& mol)
       z_max = lexicalCast<double>(box_bounds_z.at(1)).value_or(0.0);
       tilt_yz = lexicalCast<double>(box_bounds_z.at(2)).value_or(0.0);
 
-      x_min -= std::min(std::min(std::min(tilt_xy, tilt_xz), tilt_xy + tilt_xz),
-                        (double)0);
-      x_max -= std::max(std::max(std::max(tilt_xy, tilt_xz), tilt_xy + tilt_xz),
-                        (double)0);
+      auto tmp = std::minmax({ tilt_xy, tilt_xz, tilt_xy + tilt_xz, 0.0 });
+      x_min -= tmp.first;
+      x_max -= tmp.second;
       y_min -= std::min(tilt_yz, (double)0);
       y_max -= std::max(tilt_yz, (double)0);
     }
