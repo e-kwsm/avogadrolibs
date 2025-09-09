58f9327246aede8af4f3021cdfcf591fec064631
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-09-09 11:54:49 +0900

    avogadro/qtplugins/applycolors/applycolors.cpp

--- avogadro/qtplugins/applycolors/applycolors.cpp
+++ avogadro/qtplugins/applycolors/applycolors.cpp
@@ -122,7 +122,7 @@ ColormapType ApplyColors::getColormapFromString(const QString& name) const
     return ColormapType::Viridis;
   if (name == tr("Cividis", "colormap"))
     return ColormapType::Cividis;
-  else if (name == tr("Spectral", "colormap"))
+  if (name == tr("Spectral", "colormap"))
     return ColormapType::Spectral;
   else if (name == tr("Coolwarm", "colormap"))
     return ColormapType::Coolwarm;
