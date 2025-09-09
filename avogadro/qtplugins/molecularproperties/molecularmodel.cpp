55fd249caac54dae88ff62464179c99f3334f79e
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-09-09 12:06:56 +0900

    avogadro/qtplugins/molecularproperties/molecularmodel.cpp avogadro/qtplugins/propertytables/propertymodel.cpp

--- avogadro/qtplugins/molecularproperties/molecularmodel.cpp
+++ avogadro/qtplugins/molecularproperties/molecularmodel.cpp
@@ -217,7 +217,7 @@ QVariant MolecularModel::headerData(int section, Qt::Orientation orientation,
       return tr("Entropy (kcal/mol•K)");
     if (it->first == "gibbs")
       return tr("Gibbs Free Energy (kcal/mol)");
-    else if (it->first == "pointgroup")
+    if (it->first == "pointgroup")
       return tr("Point Group", "point group symmetry");
     else if (it != map.end())
       return QString::fromStdString(it->first);
