4f302a9c497d3e406db15b656e8e344c3955ea49
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-09-09 13:30:24 +0900

    tests/io/turbomoletest.cpp

--- tests/io/turbomoletest.cpp
+++ tests/io/turbomoletest.cpp
@@ -171,6 +171,42 @@ TEST(TurbomoleTest, readCellParameters)
   }
 }
 
+TEST(TurbomoleTest, readOk)
+{
+  for (const auto& s : {
+         "$periodic 1\n$cell\n6.0\n$end"s,
+         "$periodic 2\n$cell\n6.0 8.0 90.0\n$end"s,
+         "$periodic 3\n$cell\n6.0 8.0 10.0 90.0 89.0 78.0\n$end"s,
+
+         R"($periodic 0
+$cell
+  6.0 8.0 10.0 90.0 89.0 78.0
+$end
+)"s,
+
+       }) {
+    TurbomoleFormat tmol;
+    Molecule molecule;
+    EXPECT_TRUE(tmol.readString(s, molecule)) << s << '\n' << tmol.error();
+  }
+}
+
+TEST(TurbomoleTest, readErr)
+{
+  for (const auto& s : {
+         // $periodic is invalid
+         "$periodic\n$end"s,
+         "$periodic F\n$end"s,
+         "$periodic -1\n$end"s,
+         "$periodic 4\n$end"s,
+         "$periodic 1 2\n$end"s,
+       }) {
+    TurbomoleFormat tmol;
+    Molecule molecule;
+    EXPECT_FALSE(tmol.readString(s, molecule)) << s;
+  }
+}
+
 TEST(TurbomoleTest, writeString)
 {
   {
