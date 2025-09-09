9379e841be9e772528f996223ff20703457c26e9
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-09-09 12:06:36 +0900

    avogadro/qtplugins/molecularproperties/molecularmodel.cpp avogadro/qtplugins/propertytables/propertymodel.cpp

--- avogadro/qtplugins/propertytables/propertymodel.cpp
+++ avogadro/qtplugins/propertytables/propertymodel.cpp
@@ -198,7 +198,7 @@ QString formatChargeType(QString type)
     return "Lowdin";
   if (type.toLower() == "chelpg")
     return "CHELPG";
-  else if (type.toLower() == "hirshfeld")
+  if (type.toLower() == "hirshfeld")
     return "Hirshfeld";
   else
     return type;
