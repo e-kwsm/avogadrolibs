501dc5d5cd88da505967cc1e02afde4377416b4f
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-09-09 13:30:24 +0900

    tests/io/turbomoletest.cpp

--- tests/io/turbomoletest.cpp
+++ tests/io/turbomoletest.cpp
@@ -18,7 +18,72 @@
 
 using Avogadro::Core::Molecule;
 using Avogadro::Io::TurbomoleFormat;
+using namespace std::string_literals;
 
-TEST(TurbomoleTest, read)
+TEST(TurbomoleTest, readOk)
 {
+  for (const auto& s : {
+
+         R"($periodic 1
+$cell
+6.0
+$end
+)"s,
+
+         R"($periodic 2
+$cell
+  6.0 8.0 90.0
+$end
+)"s,
+
+         R"($periodic 3
+$cell
+  6.0 8.0 10.0 90.0 89.0 78.0
+$end
+)"s,
+
+         R"($periodic 0
+$cell
+  6.0 8.0 10.0 90.0 89.0 78.0
+$end
+)"s,
+
+       }) {
+    TurbomoleFormat tmol;
+    Molecule molecule;
+    EXPECT_TRUE(tmol.readString(s, molecule)) << s << '\n' << tmol.error();
+  }
+}
+
+TEST(TurbomoleTest, readErr)
+{
+  for (const auto& s : {
+
+         R"($periodic
+$end
+)"s,
+
+         R"($periodic 1 2
+$end
+)"s,
+
+         R"($periodic -1
+$end
+)"s,
+
+         R"($periodic 4
+$end
+)"s,
+
+               R"($periodic 1
+$cell
+6.0 10.0
+$end
+)"s,
+
+       }) {
+    TurbomoleFormat tmol;
+    Molecule molecule;
+    EXPECT_FALSE(tmol.readString(s, molecule)) << s;
+  }
 }
