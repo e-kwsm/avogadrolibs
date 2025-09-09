33832bb8830fb635330b6a69c975bed49db4ac4d
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-09-09 12:11:19 +0900

    avogadro/qtplugins/surfaces/surfaces.cpp

--- avogadro/qtplugins/surfaces/surfaces.cpp
+++ avogadro/qtplugins/surfaces/surfaces.cpp
@@ -801,7 +801,7 @@ ColormapType Surfaces::getColormapFromString(const QString& name) const
     return ColormapType::Viridis;
   if (name == tr("Cividis", "colormap"))
     return ColormapType::Cividis;
-  else if (name == tr("Spectral", "colormap"))
+  if (name == tr("Spectral", "colormap"))
     return ColormapType::Spectral;
   else if (name == tr("Coolwarm", "colormap"))
     return ColormapType::Coolwarm;
