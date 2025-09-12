62174d6e5fa0e057b0cd315c47ce633d277df36a
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-09-12 22:17:02 +0900

    feat(turbomole): raise error if $cell and/or $lattice appear twice
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- avogadro/io/turbomoleformat.cpp
+++ avogadro/io/turbomoleformat.cpp
@@ -127,7 +127,8 @@ bool TurbomoleFormat::read(std::istream& inStream, Core::Molecule& mol)
       }
     } else if (tokens[0] == "$cell") {
       if (hasLattice) {
-        std::cerr << "$lattice and $cell appear" << std::endl; // valid?
+        appendError("$lattice and $cell appear");
+        return false;
       }
       if (hasCell) {
         appendError("$cell appears twice");
@@ -158,7 +159,8 @@ bool TurbomoleFormat::read(std::istream& inStream, Core::Molecule& mol)
 
     } else if (tokens[0] == "$lattice") {
       if (hasCell) {
-        std::cerr << "$cell and $lattice appear" << std::endl; // valid?
+        appendError("$cell and $lattice appear");
+        return false;
       }
       if (hasLattice) {
         appendError("$lattice appears twice");
