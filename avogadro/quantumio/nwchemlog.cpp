421b016b1f610a7b583392279b7ae8253f23a3d3
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-09-13 10:16:19 +0900

    avogadro/quantumio/nwchemjson.cpp avogadro/quantumio/nwchemlog.cpp avogadro/quantumio/orca.cpp

--- avogadro/quantumio/nwchemlog.cpp
+++ avogadro/quantumio/nwchemlog.cpp
@@ -4,12 +4,18 @@
 ******************************************************************************/
 
 #include "nwchemlog.h"
+#include "core/avogadrocore.h"
+#include "core/vector.h"
+#include "core/array.h"
 
 #include <avogadro/core/elements.h>
 #include <avogadro/core/molecule.h>
 #include <avogadro/core/utilities.h>
 
+#include <cstddef>
 #include <iostream>
+#include <string>
+#include <vector>
 
 using std::string;
 using std::vector;
