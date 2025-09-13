b99ce8d061d38a37e20d4f2664b0045891c81017
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-09-13 11:02:08 +0900

    avogadro/qtplugins/propertytables/propertymodel.cpp avogadro/qtplugins/propertytables/propertytables.cpp avogadro/qtplugins/propertytables/propertyview.cpp

--- avogadro/qtplugins/propertytables/propertyview.cpp
+++ avogadro/qtplugins/propertytables/propertyview.cpp
@@ -5,6 +5,7 @@
 
 #include "propertyview.h"
 #include "core/avogadrocore.h"
+#include "propertymodel.h"
 
 #include <avogadro/core/residue.h>
 #include <avogadro/qtgui/molecule.h>
@@ -32,6 +33,12 @@
 #include <QtWidgets/QVBoxLayout>
 
 #include <QtCore/QDebug>
+#include <qtableview.h>
+#include <qnamespace.h>
+#include <qabstractitemview.h>
+#include <qforeach.h>
+#include <qkeysequence.h>
+#include <qitemselectionmodel.h>
 
 namespace Avogadro {
 
