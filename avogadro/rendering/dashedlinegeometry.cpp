83e66a2813233887b5a4a191a100f4d801f1c808
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-09-13 10:13:38 +0900

    avogadro/rendering/cylindergeometry.cpp avogadro/rendering/dashedlinegeometry.cpp avogadro/rendering/drawable.cpp avogadro/rendering/geometrynode.cpp avogadro/rendering/geometryvisitor.cpp avogadro/rendering/glrenderer.cpp avogadro/rendering/glrendervisitor.cpp avogadro/rendering/groupnode.cpp avogadro/rendering/linestripgeometry.cpp avogadro/rendering/meshgeometry.cpp avogadro/rendering/plyvisitor.cpp

--- avogadro/rendering/dashedlinegeometry.cpp
+++ avogadro/rendering/dashedlinegeometry.cpp
@@ -8,16 +8,15 @@
 #include "avogadrogl.h"
 #include "bufferobject.h"
 #include "camera.h"
-#include "scene.h"
+#include "core/types.h"
 #include "shader.h"
 #include "shaderprogram.h"
 #include "visitor.h"
 
-#include <avogadro/core/matrix.h>
 #include <avogadro/core/vector.h>
 
+#include <cstddef>
 #include <iostream>
-#include <limits>
 
 namespace {
 #include "dashedline_fs.h"
