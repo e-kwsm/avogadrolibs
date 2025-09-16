053e65593439315684473b924066de3c582ad877
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-09-16 17:13:21 +0900

    refactor: use std::string_view
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- avogadro/core/utilities.h
+++ avogadro/core/utilities.h
@@ -9,6 +9,7 @@
 #include <algorithm>
 #include <sstream>
 #include <string>
+#include <string_view>
 #include <vector>
 
 namespace Avogadro::Core {
@@ -62,7 +63,7 @@ inline bool contains(const std::string& input, const std::string& search,
  * @param search String that will be searched for.
  * @return True if the string starts with search, false otherwise.
  */
-inline bool startsWith(const std::string& input, const std::string& search)
+inline bool startsWith(std::string_view input, std::string_view search)
 {
   return input.size() >= search.size() &&
          input.compare(0, search.size(), search) == 0;
@@ -74,7 +75,7 @@ inline bool startsWith(const std::string& input, const std::string& search)
  * @param ending String that will be searched for.
  * @return True if the string ends with ending, false otherwise.
  */
-inline bool endsWith(std::string const& input, std::string const& ending)
+inline bool endsWith(std::string_view input, std::string_view ending)
 {
   if (ending.size() > input.size())
     return false;
