ece73e4d81050db209404c6c267cecbcd9d65891
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-09-20 18:16:30 +0900

    tests/io/turbomoletest.cpp

--- tests/io/turbomoletest.cpp
+++ tests/io/turbomoletest.cpp
@@ -55,6 +55,19 @@ $end
   }
 }
 
+TEST(TurbomoleTest, readPeriodic)
+{
+  for (const auto& s : {
+         "$periodic 1\n$cell\n6.0\n$end"s,
+         "$periodic 2\n$cell\n6.0 8.0 90.0\n$end"s,
+         "$periodic 3\n$cell\n6.0 8.0 10.0 90.0 89.0 78.0\n$end"s,
+       }) {
+    TurbomoleFormat tmol;
+    Molecule molecule;
+    EXPECT_TRUE(tmol.readString(s, molecule)) << s << '\n' << tmol.error();
+  }
+}
+
 TEST(TurbomoleTest, readCellParameters)
 {
   auto cell = [](unsigned periodic, const std::string& extra = "") {
@@ -204,19 +217,6 @@ TEST(TurbomoleTest, readErr)
     TurbomoleFormat tmol;
     Molecule molecule;
     EXPECT_FALSE(tmol.readString(s, molecule)) << s;
-  }
-}
-
-TEST(TurbomoleTest, readPeriodic)
-{
-  for (const auto& s : {
-         "$periodic 1\n$cell\n6.0\n$end"s,
-         "$periodic 2\n$cell\n6.0 8.0 90.0\n$end"s,
-         "$periodic 3\n$cell\n6.0 8.0 10.0 90.0 89.0 78.0\n$end"s,
-       }) {
-    TurbomoleFormat tmol;
-    Molecule molecule;
-    EXPECT_TRUE(tmol.readString(s, molecule)) << s << '\n' << tmol.error();
   }
 }
 
