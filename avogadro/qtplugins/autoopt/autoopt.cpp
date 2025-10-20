e0118c227d0aa325c3d8b0d9398e32b7037245b0
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-10-21 04:54:59 +0900

    avogadro/qtplugins/autoopt/autoopt.cpp

--- avogadro/qtplugins/autoopt/autoopt.cpp
+++ avogadro/qtplugins/autoopt/autoopt.cpp
@@ -770,7 +770,7 @@ void AutoOpt::translate(Vector3 delta, bool moveSelected)
   for (Index i = 0; i < m_molecule->atomCount(); ++i) {
     if (moveSelected && !m_molecule->atomSelected(i))
       continue;
-    else if (!moveSelected && m_molecule->atomSelected(i))
+    if (!moveSelected && m_molecule->atomSelected(i))
       continue;
 
     Vector3 currentPos = m_molecule->atomPosition3d(i);
@@ -800,7 +800,7 @@ void AutoOpt::rotate(Vector3 delta, Vector3 centroid, bool moveSelected)
   for (Index i = 0; i < m_molecule->atomCount(); ++i) {
     if (moveSelected && !m_molecule->atomSelected(i))
       continue;
-    else if (!moveSelected && m_molecule->atomSelected(i))
+    if (!moveSelected && m_molecule->atomSelected(i))
       continue;
 
     Vector3 currentPos = m_molecule->atomPosition3d(i);
@@ -816,7 +816,7 @@ void AutoOpt::axisRotate(Vector3 delta, Vector3 centroid, bool moveSelected)
   for (Index i = 0; i < m_molecule->atomCount(); ++i) {
     if (moveSelected && !m_molecule->atomSelected(i))
       continue;
-    else if (!moveSelected && m_molecule->atomSelected(i))
+    if (!moveSelected && m_molecule->atomSelected(i))
       continue;
 
     Vector3 currentPos = m_molecule->atomPosition3d(i);
