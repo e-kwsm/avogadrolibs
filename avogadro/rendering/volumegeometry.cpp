53e9eb19f6d0e78d109e8139b4736932f78c59aa
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-11-01 11:41:13 +0900

    modernize-use-nullptr
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- avogadro/rendering/volumegeometry.cpp
+++ avogadro/rendering/volumegeometry.cpp
@@ -433,7 +433,7 @@ void VolumeGeometry::initialize()
                  GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
-                          (GLvoid*)0);
+                          (GLvoid*)nullptr);
 
     glGenBuffers(1, &d->volumeBoxEbo);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, d->volumeBoxEbo);
@@ -573,7 +573,7 @@ void VolumeGeometry::render(const Camera& camera)
     d->boxShaders->setUniformValue("uBoxMax", boxMax);
 
     glBindVertexArray(d->volumeBoxVao);
-    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, nullptr);
     glBindVertexArray(0);
 
     d->boxShaders->release();
@@ -602,7 +602,7 @@ void VolumeGeometry::render(const Camera& camera)
     d->boxShaders->setUniformValue("uBoxMax", boxMax);
 
     glBindVertexArray(d->volumeBoxVao);
-    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, nullptr);
     glBindVertexArray(0);
 
     d->boxShaders->release();
