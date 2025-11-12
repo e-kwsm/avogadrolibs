f949d21e819e92e5352ef2c2248dc24b74dbb589
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-11-12 18:44:01 +0900

    ConstraintsDialog
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- avogadro/qtplugins/constraints/constraintsdialog.h
+++ avogadro/qtplugins/constraints/constraintsdialog.h
@@ -42,7 +42,7 @@ public slots:
   void updateConstraints();
 
 private:
-  Ui::ConstraintsDialog* ui;
+  std::unique_ptr<Ui::ConstraintsDialog> ui;
   ConstraintsModel* m_model;
   QtGui::Molecule* m_molecule = nullptr;
 };
