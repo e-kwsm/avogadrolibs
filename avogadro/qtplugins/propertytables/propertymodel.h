30ce0a8bcf3fc7374afc27f603d0fe3710bddc7a
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-11-27 12:31:33 +0900

    avogadro/qtplugins/propertytables/propertymodel.h

--- avogadro/qtplugins/propertytables/propertymodel.h
+++ avogadro/qtplugins/propertytables/propertymodel.h
@@ -57,7 +57,7 @@ public:
   void setMolecule(QtGui::Molecule* molecule);
 
   // Return what type of model this is
-  PropertyType type() const { return m_type; };
+  PropertyType type() const { return m_type; }
   bool isColorIndex(const QModelIndex& index) const;
 
   // Partial charge type selection
