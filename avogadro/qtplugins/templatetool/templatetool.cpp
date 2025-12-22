e35853be2fd97a6585c840dc66605e81a1b24597
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-12-22 09:05:14 +0900

    avogadro/qtplugins/propertytables/propertymodel.cpp avogadro/qtplugins/selectiontool/selectiontool.cpp avogadro/qtplugins/templatetool/templatetool.cpp

--- avogadro/qtplugins/templatetool/templatetool.cpp
+++ avogadro/qtplugins/templatetool/templatetool.cpp
@@ -61,9 +61,6 @@ namespace Avogadro {
 namespace QtPlugins {
 
 using QtGui::Molecule;
-using QtGui::RWAtom;
-using QtGui::RWBond;
-using QtGui::RWMolecule;
 using QtOpenGL::GLWidget;
 
 using Avogadro::Core::Elements;
