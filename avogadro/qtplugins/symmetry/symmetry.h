d12a7d767c5bc6dbd3ee9227a85b4f0df273fefb
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-01-09 19:06:52 +0900

    refactor(Symmetry::m_viewSymmetryAction): unique_ptr
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- avogadro/qtplugins/symmetry/symmetry.h
+++ avogadro/qtplugins/symmetry/symmetry.h
@@ -10,6 +10,8 @@
 
 #include "symmetrywidget.h"
 
+#include <memory>
+
 namespace msym {
 extern "C"
 {
@@ -85,7 +87,7 @@ private:
   QtGui::Molecule* m_molecule;
   SymmetryWidget* m_symmetryWidget;
 
-  QAction* m_viewSymmetryAction;
+  std::unique_ptr<QAction> m_viewSymmetryAction;
 
   msym::msym_context m_ctx;
 
