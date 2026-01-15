06e1fb18a48166f4789533e59ad490ae2454a38e
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-01-15 20:08:58 +0900

    avogadro/qtplugins/playertool/playertool.cpp

--- avogadro/qtplugins/playertool/playertool.cpp
+++ avogadro/qtplugins/playertool/playertool.cpp
@@ -43,9 +43,9 @@ using QtGui::Molecule;
 PlayerTool::PlayerTool(QObject* parent_)
   : QtGui::ToolPlugin(parent_), m_activateAction(new QAction(this)),
     m_molecule(nullptr), m_renderer(nullptr), m_currentFrame(0),
-    m_toolWidget(nullptr), m_frameIdx(nullptr), m_firstFrameIdx(nullptr),
-    m_lastFrameIdx(nullptr), m_slider(nullptr), m_dynamicBonding(nullptr),
-    m_glWidget(nullptr), m_timer(this), m_animationFPS(nullptr),
+    m_toolWidget(nullptr), m_timer(this), m_animationFPS(nullptr),
+    m_frameIdx(nullptr), m_firstFrameIdx(nullptr), m_lastFrameIdx(nullptr),
+    m_dynamicBonding(nullptr), m_glWidget(nullptr), m_slider(nullptr),
     playButton(nullptr)
 {
   QString shortcut = tr("Ctrl+9", "control-key 9");
