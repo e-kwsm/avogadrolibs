da8279c6740aab3afa3d8131c973870612e116fc
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-01-16 11:18:57 +0900

    avogadro/quantumio/gamessus.cpp avogadro/quantumio/orca.cpp

--- avogadro/quantumio/orca.cpp
+++ avogadro/quantumio/orca.cpp
@@ -10,6 +10,7 @@
 #include <avogadro/core/utilities.h>
 
 #include <algorithm>
+#include <cmath>
 #include <cstddef>
 #include <memory>
 #include <iostream>
@@ -537,7 +538,7 @@ void ORCAOutput::processLine(std::istream& in,
   } else {
 
     vector<vector<double>> columns;
-    unsigned int numColumns, numRows;
+    unsigned int numColumns = 0, numRows = 0;
     numColumns = 0;
     numRows = 0;
     // number of basis functions (rows) per MO block, learned from the first
@@ -955,7 +956,7 @@ void ORCAOutput::processLine(std::istream& in,
         if (key.empty())
           break;
         list = Core::split(key, ' ');
-        double wavenumbers;
+        double wavenumbers = NAN;
         while (!key.empty()) {
           // should have 8 columns
           if (list.size() < 8) {
@@ -1057,7 +1058,7 @@ void ORCAOutput::processLine(std::istream& in,
           break;
         nGTOs = 0;
         list = Core::split(key, ' ');
-        int nShells;
+        int nShells = 0;
         // init all vectors etc.
         m_basisAtomLabel.clear();
         m_orcaNumShells.resize(0);
@@ -1433,7 +1434,7 @@ void ORCAOutput::parseMCD()
 
   // read in line by line
   // frequency, mcd, absorption
-  Real frequency, mcd, absorption;
+  Real frequency = NAN, mcd = NAN, absorption = NAN;
   frequency = mcd = absorption = 0.0;
   while (in) {
     in >> frequency >> mcd >> absorption;
