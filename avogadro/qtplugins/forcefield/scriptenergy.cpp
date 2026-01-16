4578b1e328c2f4f6ddc0ef83cfbbbc4fb48a2be6
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-01-16 11:27:46 +0900

    avogadro/qtplugins/forcefield/scriptenergy.cpp

--- avogadro/qtplugins/forcefield/scriptenergy.cpp
+++ avogadro/qtplugins/forcefield/scriptenergy.cpp
@@ -389,7 +389,7 @@ void ScriptEnergy::processElementString(const QString& str)
         return;
 
       // get the two numbers
-      bool ok;
+      bool ok = false;
       int start = strList2[0].toInt(&ok);
       if (!ok || start < 1 || start > 119)
         return;
@@ -400,7 +400,7 @@ void ScriptEnergy::processElementString(const QString& str)
         m_elements.set(i);
     }
 
-    bool ok;
+    bool ok = false;
     int i = sstr.toInt(&ok);
     if (!ok || i < 1 || i > 119)
       return;
