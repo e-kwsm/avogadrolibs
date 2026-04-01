4d56b597e30ecb70dc39e8d3ccdcf589bb064df2
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-04-02 00:20:13 +0900

    tests/core/moleculetest.cpp

--- tests/core/moleculetest.cpp
+++ tests/core/moleculetest.cpp
@@ -1048,7 +1048,8 @@ TEST_F(MoleculeTest, formulaCompositionUnitCellCorner)
   Molecule molecule;
 
   // Create a simple cubic unit cell (10 Angstrom sides)
-  UnitCell* cell = new UnitCell(10.0, 10.0, 10.0, M_PI / 2, M_PI / 2, M_PI / 2);
+  auto cell =
+    std::make_shared<UnitCell>(10.0, 10.0, 10.0, M_PI / 2, M_PI / 2, M_PI / 2);
   molecule.setUnitCell(cell);
 
   // Add 8 corner atoms (each at a corner of the unit cell)
@@ -1073,7 +1074,8 @@ TEST_F(MoleculeTest, formulaCompositionUnitCellEdge)
   Molecule molecule;
 
   // Create a simple cubic unit cell (10 Angstrom sides)
-  UnitCell* cell = new UnitCell(10.0, 10.0, 10.0, M_PI / 2, M_PI / 2, M_PI / 2);
+  auto cell =
+    std::make_shared<UnitCell>(10.0, 10.0, 10.0, M_PI / 2, M_PI / 2, M_PI / 2);
   molecule.setUnitCell(cell);
 
   // Add 4 edge atoms (each on an edge of the unit cell, 2 coords at 0 or 1)
@@ -1100,7 +1102,8 @@ TEST_F(MoleculeTest, formulaCompositionUnitCellFace)
   Molecule molecule;
 
   // Create a simple cubic unit cell (10 Angstrom sides)
-  UnitCell* cell = new UnitCell(10.0, 10.0, 10.0, M_PI / 2, M_PI / 2, M_PI / 2);
+  auto cell =
+    std::make_shared<UnitCell>(10.0, 10.0, 10.0, M_PI / 2, M_PI / 2, M_PI / 2);
   molecule.setUnitCell(cell);
 
   // Add 2 face atoms (each on a face of the unit cell, 1 coord at 0 or 1)
@@ -1125,7 +1128,8 @@ TEST_F(MoleculeTest, formulaCompositionUnitCellInterior)
   Molecule molecule;
 
   // Create a simple cubic unit cell (10 Angstrom sides)
-  UnitCell* cell = new UnitCell(10.0, 10.0, 10.0, M_PI / 2, M_PI / 2, M_PI / 2);
+  auto cell =
+    std::make_shared<UnitCell>(10.0, 10.0, 10.0, M_PI / 2, M_PI / 2, M_PI / 2);
   molecule.setUnitCell(cell);
 
   // Add an interior atom (no coords at 0 or 1)
@@ -1142,7 +1146,8 @@ TEST_F(MoleculeTest, formulaCompositionUnitCellMixed)
   Molecule molecule;
 
   // Create a simple cubic unit cell (10 Angstrom sides)
-  UnitCell* cell = new UnitCell(10.0, 10.0, 10.0, M_PI / 2, M_PI / 2, M_PI / 2);
+  auto cell =
+    std::make_shared<UnitCell>(10.0, 10.0, 10.0, M_PI / 2, M_PI / 2, M_PI / 2);
   molecule.setUnitCell(cell);
 
   // NaCl rock salt structure simulation:
