1c673f2803118190d79592d673567587cf15bab4
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-04-02 11:03:05 +0900

    avogadro/qtgui/extensionplugin.h avogadro/qtplugins/constraints/constraintsextension.cpp avogadro/qtplugins/constraints/constraintsextension.h

--- avogadro/qtgui/extensionplugin.h
+++ avogadro/qtgui/extensionplugin.h
@@ -13,6 +13,8 @@
 #include <QtCore/QObject>
 #include <QtCore/QVariantMap>
 
+#include <memory>
+
 class QAction;
 
 namespace Avogadro {
@@ -85,7 +87,7 @@ public slots:
   /**
    * Called when the current molecule changes.
    */
-  virtual void setMolecule(QtGui::Molecule* mol) = 0;
+  virtual void setMolecule(const std::shared_ptr<QtGui::Molecule>& mol) = 0;
 
   /**
    * Call this slot when the moleculeReady signal indicated that a molecule is
