2132f118d19c8f3c43ad88d373d7e9a6cdd1a8f2
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-04-04 18:08:22 +0900

    avogadro/qtplugins/lammpsinput/lammpsinput.cpp avogadro/qtplugins/lammpsinput/lammpsinput.h avogadro/qtplugins/lineformatinput/lineformatinput.cpp avogadro/qtplugins/lineformatinput/lineformatinput.h

--- avogadro/qtplugins/lineformatinput/lineformatinput.h
+++ avogadro/qtplugins/lineformatinput/lineformatinput.h
@@ -34,7 +34,7 @@ public:
   QStringList menuPath(QAction*) const override;
 
 public slots:
-  void setMolecule(QtGui::Molecule*) override;
+  void setMolecule(const std::shared_ptr<QtGui::Molecule>&) override;
 
 private slots:
   void showDialog();
