017602a64df0f4b4fca2b46c10223cf152b3dbd8
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-04-04 17:49:06 +0900

    avogadro/qtplugins/alchemy/alchemy.cpp avogadro/qtplugins/alchemy/alchemy.h avogadro/qtplugins/apbs/apbs.h

--- avogadro/qtplugins/alchemy/alchemy.h
+++ avogadro/qtplugins/alchemy/alchemy.h
@@ -40,7 +40,7 @@ public:
   QStringList menuPath(QAction* action) const override;
 
 public slots:
-  void setMolecule(QtGui::Molecule* mol) override;
+  void setMolecule(const std::shared_ptr<QtGui::Molecule>& mol) override;
 
 private slots:
   void changeElements();
