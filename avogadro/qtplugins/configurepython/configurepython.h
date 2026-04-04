13119613a6e2f73a802135bf067ed1991e21d13e
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-04-04 17:58:06 +0900

    avogadro/qtplugins/bonding/bonding.cpp avogadro/qtplugins/centroid/centroid.cpp avogadro/qtplugins/centroid/centroid.h avogadro/qtplugins/configurepython/configurepython.h

--- avogadro/qtplugins/configurepython/configurepython.h
+++ avogadro/qtplugins/configurepython/configurepython.h
@@ -33,7 +33,7 @@ public:
 
   QStringList pythonPaths() const;
 
-  void setMolecule(QtGui::Molecule*) override {}
+  void setMolecule(const std::shared_ptr<QtGui::Molecule>&) override {}
 
 private slots:
   void showDialog();
