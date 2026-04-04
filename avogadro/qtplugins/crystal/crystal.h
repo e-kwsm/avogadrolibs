f1c67ddb58992684a83c9ab7d12e4b55a53e8668
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-04-04 18:01:35 +0900

    avogadro/qtplugins/crystal/crystal.cpp avogadro/qtplugins/crystal/crystal.h avogadro/qtplugins/customelements/customelements.cpp avogadro/qtplugins/customelements/customelements.h

--- avogadro/qtplugins/crystal/crystal.h
+++ avogadro/qtplugins/crystal/crystal.h
@@ -33,7 +33,7 @@ public:
   void registerCommands() override;
 
 public slots:
-  void setMolecule(QtGui::Molecule* mol) override;
+  void setMolecule(const std::shared_ptr<QtGui::Molecule>& mol) override;
 
   void moleculeChanged(unsigned int changes);
 
