90dfc34684da5a1d7f1b1bef47e92254edec5d62
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-04-04 18:07:22 +0900

    avogadro/qtplugins/insertfragment/insertfragment.cpp avogadro/qtplugins/insertfragment/insertfragment.h avogadro/qtplugins/insertpeptide/insertpeptide.cpp avogadro/qtplugins/insertpeptide/insertpeptide.h

--- avogadro/qtplugins/insertpeptide/insertpeptide.h
+++ avogadro/qtplugins/insertpeptide/insertpeptide.h
@@ -42,7 +42,7 @@ public:
   QStringList menuPath(QAction*) const override;
 
 public slots:
-  void setMolecule(QtGui::Molecule*) override;
+  void setMolecule(const std::shared_ptr<QtGui::Molecule>&) override;
 
 private slots:
   void showDialog();
