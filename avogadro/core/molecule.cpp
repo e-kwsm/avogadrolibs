a63d3b7b25fc6d351fc85affaa69aaa6fb54095a
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-08-18 00:42:29 +0900

    fixup! refactor: modernize-loop-convert
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- avogadro/core/molecule.cpp
+++ avogadro/core/molecule.cpp
@@ -672,7 +672,7 @@ signed char Molecule::totalCharge() const
   if (m_data.hasValue("totalCharge")) {
     charge = m_data.value("totalCharge").toInt();
   } else if (m_formalCharges.size() > 0) {
-    for (signed char m_formalCharge : m_formalCharges)
+    for (auto m_formalCharge : m_formalCharges)
       charge += m_formalCharge;
     return charge;
   }
@@ -689,7 +689,7 @@ char Molecule::totalSpinMultiplicity() const
   } else {
     // add up the electrons
     unsigned long electrons = 0;
-    for (unsigned char m_atomicNumber : m_atomicNumbers)
+    for (auto m_atomicNumber : m_atomicNumbers)
       electrons += m_atomicNumber;
 
     // adjust by the total charge
--- avogadro/qtplugins/templatetool/templatetoolwidget.cpp
+++ avogadro/qtplugins/templatetool/templatetoolwidget.cpp
@@ -685,7 +685,7 @@ void TemplateToolWidget::buildElements()
 void TemplateToolWidget::saveElements()
 {
   QVariantList atomicNums;
-  for (unsigned char m_userElement : m_userElements)
+  for (auto m_userElement : m_userElements)
     atomicNums << QVariant(m_userElement);
 
   QSettings().setValue("templatetool/userElements", atomicNums);
