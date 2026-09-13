ce42a69c244764ed08063a154df14f1684067325
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-09-13 20:24:26 +0900

    fixup! refactor: cppcoreguidelines-use-default-member-init
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- avogadro/io/fileformat.cpp
+++ avogadro/io/fileformat.cpp
@@ -37,11 +37,7 @@ json parseOptions(const std::string& options)
 
 } // namespace
 
-FileFormat::FileFormat()
-  : m_mode(None), m_in(nullptr), m_out(nullptr), m_decompressor(nullptr),
-    m_compressor(nullptr), m_outputError(false)
-{
-}
+FileFormat::FileFormat() {}
 
 FileFormat::~FileFormat()
 {
