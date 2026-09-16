4f54a0e1c0bca809a3dd7020f52346a0e1949252
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2026-09-17 08:38:46 +0900

    fixup! perf: performance-noexcept-swap
    Signed-off-by: Eisuke Kawashima <e-kwsm@users.noreply.github.com>

--- avogadro/rendering/volumegeometry.h
+++ avogadro/rendering/volumegeometry.h
@@ -24,7 +24,7 @@ public:
   ~VolumeGeometry() override;
 
   VolumeGeometry& operator=(VolumeGeometry);
-  friend void swap(VolumeGeometry& lhs, VolumeGeometry& rhs) noexcept;
+  friend void swap(VolumeGeometry& lhs, VolumeGeometry& rhs);
 
   /**
    * Accept a visit from our friendly visitor.
